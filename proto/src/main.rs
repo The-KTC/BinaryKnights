1cc6249f6530bf5454fd304886804f1e17da5150
The_KTC <91944251+The-KTC@users.noreply.github.com>
2024-06-17 18:39:38 +0200

    buttons can print textfiles in console

--- proto/src/main.rs
+++ proto/src/main.rs
@@ -5,8 +5,6 @@ use crypto_layer::common::factory::{SecModules, SecurityModule};
 use crypto_layer::tpm::core::instance::TpmType;
 use crypto_layer::tpm::macos::logger::Logger;
 use crypto_layer::tpm::macos::SecureEnclaveConfig;
-// use slint::slint;
-// use slint::SharedString;
 
 slint::include_modules!();
 
@@ -24,19 +22,26 @@ fn main() -> Result<(), slint::PlatformError> {
     let hash = Hash::Sha2(Sha2Bits::Sha224);
     let config: SecureEnclaveConfig = SecureEnclaveConfig::new(Some(key_algorithm), Some(hash));
     
-    
-
-
     let app_ui = AppWindow::new()?;
-    // app_ui.set_encryptedValue("Key created successful".into());
-    // let ui_handle = ui.as_weak();
-    // ui.on_divide_income(move |string| {
-    //     let ui = ui_handle.unwrap();
-    let ui = app_ui.as_weak().unwrap();
-    app_ui.on_request_generate_keys(move || {
-        
+    let mut ui = app_ui.as_weak().unwrap();
+
+    app_ui.on_encrypt(move |string| {
+        println!("{}",string.trim());
     });
-            
+    app_ui.on_decrypt(move |string| {
+        println!("{}",string.trim());
+    });
+    app_ui.on_sign(move |string| {
+        println!("{}",string.trim());
+    });
+    app_ui.on_verifyData(move |string| {
+        println!("{}",string.trim());
+    });
+    app_ui.on_reset(move || {
+        println!("RESET LoL");
+    });
+
+    // ui = app_ui.as_weak().unwrap();
     match tpm_provider
         .lock()
         .unwrap()
@@ -45,26 +50,5 @@ fn main() -> Result<(), slint::PlatformError> {
         Ok(()) => {ui.set_statusKey("Key created successful".into()); println!("Key created successful");},
         Err(e) => {ui.set_statusKey(format!("Failed to create key: {:?}", e).into()); println!("{}",format!("Failed to create key: {:?}", e));},
     }
-
-
-    // match app_ui {
-    //     Ok(window) => {
-    //         window.on_request_generate_keys(move || {
-    //             // appUI.set_encryptedValue("TEST");
-    //             match tpm_provider
-    //                 .lock()
-    //                 .unwrap()
-    //                 .create_key(key_id, Box::new(config.clone()))
-    //             {
-    //                 Ok(()) => app_ui.set_encryptedValue("Key created successful".into()),
-    //                 Err(e) => app_ui.set_encryptedValue(("Failed to create key: {:?}", e).into()),
-    //             }
-    //         });
-    //         if let Err(e) = window.run() {
-    //             eprintln!("Failed to run the app window: {:?}", e);
-    //         }
-    //     }
-    //     Err(e) => eprintln!("Failed to create app window: {:?}", e),
-    // }
     app_ui.run()
 }
